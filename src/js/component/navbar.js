ed42887973c0b6d80022fb0fc5186ef09d99f18e
xfontainex <maralafontaine@gmail.com>
2022-12-30 00:06:02 +0000

    adding files to repo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,56 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		// <!-- nav bar -->
+		
+        <nav className="navbar navbar-expand-lg bg-light my-4">
+            <div className="container-fluid justify-content-end">
+
+                {/* <!-- Brand --> */}
+                <h1 className="navbar-brand">Here4U L.A.</h1>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                {/* <!-- Nav-bar content --> */}
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    {/* <!-- Disabled nav bar content --> */}
+                    {/* <!-- <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#"> Map</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="emergency" href="#"> Emergency</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"> Contact </a>
+                        </li>
+                    </ul> --> */}
+
+
+
+                    {/* <!-- English/Spanish --> */}
+                    <div className="btn-groups btn-group-toggle" data-toggle="buttons">
+                        <label className="btn btn-light">
+                            <input type="radio" name="options" id="option1" autocomplete="off" checked/> English
+                        </label>
+                        <label className="btn btn-light">
+                            <input type="radio" name="options" id="option2" autocomplete="off"/> Español
+                        </label>
+                    </div>
+
+                    {/* <!-- Donate --> */}
+                    <button type="button" className="btn btn-outline-primary me-1">Donate</button>
+
+                    {/* <!-- Emergency Resources --> */}
+                    <button type="button" className="btn btn-outline-danger me-1">Emergency Resources</button>
+
+                    {/* <!-- Login --> */}
+                    <button type="button" className="btn btn-outline-primary"> Login </button>
+                </div>
+            </div>
+        </nav>
 	);
 };
